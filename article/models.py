4ef9f42970e9dfa72c10d63549ec2ccf90999b4d
Anny Devoy <annydevoyalt@gmail.com>
2024-10-07 20:51:18 +0000

    Add article model - without cloudinary field

--- article/models.py
+++ article/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+TAGS = ((0, "None"), (1, "Course"), (2, "Tools"), (3, "Project"), (4, "Wood"), (5, "How-to"))
+STATUS = ((0, "Draft"), (1, "Published"))
+
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="article_posts")
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    pull_quote = models.CharField(max_length=200, unique=True)
+    tags = models.IntegerField(choices=TAGS, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
