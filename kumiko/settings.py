e1333dce9aacad1dd8d94dacf074a763af920445
Anny Devoy <annydevoyalt@gmail.com>
2024-10-07 17:37:43 +0000

    Fix Procfile typo

--- kumiko/settings.py
+++ kumiko/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_^_et1+8f3n&1^e3um0kmb0!o2437=z2*zss*pv*li$kc-%k(m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-annytomica-kumikocommun-qzmdjguowe5.ws.codeinstitute-ide.net','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-annytomica-kumikocommun-qzmdjguowe5.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
