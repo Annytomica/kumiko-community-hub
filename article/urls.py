8e32ba17fe03c9aa72872388dd0d279346754e35
Anny Devoy <annydevoyalt@gmail.com>
2024-10-10 17:46:50 +0000

    Add single article page - no abstract styling

--- article/urls.py
+++ article/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     # temporary name = 'home', change back to articles once real homepage created
     path('articles/', views.ArticleList.as_view(), name='home'),
+    path('<slug:slug>/', views.single_article, name='single_article'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Article
 
@@ -8,3 +8,27 @@ class ArticleList(generic.ListView):
     queryset = Article.objects.filter(status=1).order_by("-created_on")
     template_name = "article/index.html"
     paginate_by = 1
+
+
+def single_article(request, slug):
+    """
+    Display an individual :model:`article.Article`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`article.Article`.
+
+    **Template:**
+
+    :template:`article/single_article.html`
+    """
+
+    queryset = Article.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "article/single_article.html",
+        {"article": single_article},
+    )
