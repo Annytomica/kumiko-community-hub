a77aa4968ff7f6ba6d4ce5632d9f765795e32c0f
Anny Devoy <annydevoyalt@gmail.com>
2024-10-11 20:15:34 +0000

    Add edit functionality to article comments - with authourisation

--- article/urls.py
+++ article/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     # temporary name = 'home', change back to articles once real homepage created
     path('articles/', views.ArticleList.as_view(), name='home'),
     path('<slug:slug>/', views.single_article, name='single_article'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.article_comment_edit, name='article_comment_edit'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
-from .models import Article
+from django.http import HttpResponseRedirect
+from .models import Article, ArticleComment
 from .forms import ArticleCommentForm
 
 
@@ -54,3 +55,26 @@ def single_article(request, slug):
         "article_comment_form": article_comment_form,
         },
     )
+
+
+def article_comment_edit(request, slug, comment_id):
+    """
+    view to edit article comments
+    """
+    if request.method == "POST":
+
+        queryset = Article.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(ArticleComment, pk=comment_id)
+        comment_form = ArticleCommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('single_article', args=[slug]))
