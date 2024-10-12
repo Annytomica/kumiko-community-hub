42a1693c4fada883177f315d08473bc1cc35e54c
Anny Devoy <annydevoyalt@gmail.com>
2024-10-12 17:55:49 +0000

    Add contactMessage model to contact app

--- contact/admin.py
+++ contact/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Contact
+from .models import Contact, ContactMessage
 
 
-
+# Register your models here.
 @admin.register(Contact)
 class ContactAdmin(SummernoteModelAdmin):
     list_display = ('opening', )
     summernote_fields = ()
 
 
-# Register your models here.
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('message', 'read',)
--- contact/models.py
+++ contact/models.py
@@ -2,7 +2,11 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
 class Contact(models.Model):
+    """ 
+    Stores single about me content
+    """
     opening = models.CharField(max_length=200)
     body = models.TextField()
     contact_image = CloudinaryField('image', default='placeholder')
@@ -11,3 +15,19 @@ class Contact(models.Model):
     def __str__(self):
         return self.opening
 
+
+class ContactMessage(models.Model):
+    """
+    Stores a single contact request message
+    """
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"Message from {self.name} on {self.created_on}"
