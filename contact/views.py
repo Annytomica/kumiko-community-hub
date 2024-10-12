7bb29f9fbe8535097b7351b3e92f1886e3dbe76d
Anny Devoy <annydevoyalt@gmail.com>
2024-10-12 18:57:07 +0000

    Add form to contact page

--- contact/views.py
+++ contact/views.py
@@ -1,16 +1,44 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import Contact
+from .forms import ContactMessage
 
 # Create your views here.
 def contact_me(request):
+    """ 
+    Renders the most recent information about the website
+    and allows user feedback
+    Displays an individual instance of :model:`contact.Contact`.
+    
+    **Context**
+    ``contact``
+        The most recent instance of :model:`contact.Contact`.
+    ``contact_message``
+        An instance of :form`contact.ContactMessage`.
+    **Template**
+    :template:`contact/contact.html`
+    """
+
+    if request.method == "POST":
+        contact_message = ContactMessage(data=request.POST)
+        if contact_message.is_valid():
+            contact_message.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                "Feedback received! Thank You!")
+    
+    
     """
     Renders the About page
     """
     contact = Contact.objects.all().order_by('-updated_on').first()
 
+    contact_message = ContactMessage()
+
     return render(
         request,
         "contact/contact.html",
-        {"contact": contact},
+        {"contact": contact,
+        "contact_message": contact_message,},
     )
 
